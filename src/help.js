3ab181b6bcf2f7799074e6d69152179292849246
a1cd <71281043+a1cd@users.noreply.github.com>
2021-06-19 21:17:19 -0400

    added help file

--- src/help.js
+++ src/help.js
@@ -1,1 +1,7 @@
+const {Cmd} = require('discord-made-siple');
 
+
+class help extends Cmd {
+    
+    help = "help [command] \nshows the help page of a command or subcommand. If no command is supplied, shows the descriptions of all neighboring commands and descriptions."
+}
