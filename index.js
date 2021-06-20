52131e799bfa1d694aee328ee95d723b8dcc79a6
a1cd <71281043+a1cd@users.noreply.github.com>
2021-06-20 17:55:50 -0400

    add nick command

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const {ping} = require('./src/ping');
 const {uptime} = require('./src/uptime');
 const {Cmd, help} = require('discord-made-siple');
+const nick = require("./src/name")
 
 module.exports = {
     help,
     ping,
-    uptime
+    uptime,
+    nick
 }
